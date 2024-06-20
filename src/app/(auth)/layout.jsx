c13827278af077cb3681a707209a02ccd2b0502b
Deepesh Gupta <34032722+deepesh-dg@users.noreply.github.com>
2024-06-20 19:36:58 +0530

    login layout set

--- src/app/(auth)/layout.jsx
+++ src/app/(auth)/layout.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
+import { useAuth } from "@/store";
+import BG_IMG from "@/assets/img/login-bg.png";
 
 export default function AuthLayout() {
-    let auth = false;
+    const { isAuthenticated } = useAuth();
 
-    return auth ? (
+    return isAuthenticated ? (
         <Navigate to="/" />
     ) : (
-        <div>
-            AuthLayout
+        <div className="relative flex min-h-screen w-full flex-col items-center justify-center bg-cover bg-center bg-no-repeat py-20 backdrop-blur-sm">
+            <div
+                className="absolute inset-0 h-full w-full blur-sm"
+                style={{
+                    backgroundImage: `url(${BG_IMG})`,
+                }}
+            />
             <Outlet />
         </div>
     );
--- src/app/(default-layout)/route.jsx
+++ src/app/(default-layout)/route.jsx
@@ -1,12 +1,6 @@
 import DefaultLayout from "./layout";
 import Home from "./page";
 import ProtectedRoutes from "./(protected)/route";
-import Login from "./login";
-import Signup from "./signup";
-import Location from "./location";
-import MembershipPlan from "./membershipPlan";
-import OtpVarification from "./otpVarification";
-import UserRole from "./userRole"
 /**
  * @type {import('react-router-dom').RouteObject[]}
  */
@@ -18,30 +12,6 @@ const DefaultRoutes = [
             {
                 path: "/",
                 element: <Home />,
-            },
-            {
-                path: "/login",
-                element: <Login />,
-            },
-            {
-                path: "/signup",
-                element: <Signup />,
-            },
-            {
-                path: "/location",
-                element: <Location />,
-            },
-            {
-                path: "/membership-plan",
-                element: <MembershipPlan />,
-            },
-            {
-                path: "/otp-varification",
-                element: <OtpVarification />,
-            },
-            {
-                path: "/user-role",
-                element: <UserRole />,
             },
             ...ProtectedRoutes,
         ],
