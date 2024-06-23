600b7caf9053d76cccde5faaa22672a1415f3332
krishna5867 <krishnakmr968@gmail.com>
2024-06-23 15:02:39 +0530

    solved conflicts

--- src/components/icons/outline.js
+++ src/components/icons/outline.js
@@ -17,22 +17,4 @@ import SearchIcon from "@/assets/icons/outline/search.svg"
 import CameraDpIcon from "@/assets/icons/outline/camera-dp.svg"
 
 
-
-import MailIcon from "@/assets/icons/outline/mail.svg";
-import UserIcon from "@/assets/icons/outline/user.svg";
-import PasswordIcon from "@/assets/icons/outline/password.svg";
-import EyeIcon from "@/assets/icons/outline/eye.svg";
-import LocationIcon from "@/assets/icons/outline/location.svg";
-import CameraIcon from "@/assets/icons/outline/camera.svg";
-import UserDpIcon from "@/assets/icons/outline/userDp.svg";
-import ClosecircleIcon from "@/assets/icons/outline/closecircle.svg";
-import TickcircleIcon from "@/assets/icons/outline/tickcircle.svg";
-import ArrowDownIcon from "@/assets/icons/outline/arrowdown.svg";
-import BellIcon from "@/assets/icons/outline/bell.svg";
-import MessageIcon from "@/assets/icons/outline/message.svg";
-import UserRoundedIcon from "@/assets/icons/outline/userRounded.svg";
-import LogoIcon from "@/assets/icons/outline/logo.svg";
-import HamburgerMenuIcon from "@/assets/icons/outline/hamburger.svg";
-import SearchIcon from "@/assets/icons/outline/search.svg";
-
 export { MailIcon, UserIcon, PasswordIcon, EyeIcon, LocationIcon, CameraIcon, UserDpIcon, ClosecircleIcon, TickcircleIcon, ArrowDownIcon, BellIcon, MessageIcon, UserRoundedIcon, LogoIcon, HamburgerMenuIcon, SearchIcon, CameraDpIcon }
