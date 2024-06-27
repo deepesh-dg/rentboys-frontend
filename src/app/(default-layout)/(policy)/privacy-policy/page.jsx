9f8b08cca79dc53574ace04745cbafeaab7ea58a
krishna5867 <krishnakmr968@gmail.com>
2024-06-28 03:37:01 +0530

    privacy-policy, cookies, dmca-notice,  T&C pages

--- src/app/(default-layout)/(policy)/privacy-policy/page.jsx
+++ src/app/(default-layout)/(policy)/privacy-policy/page.jsx
@@ -6,7 +6,110 @@ const page = () => {
             <h2 className="border-b border-gray-50 pb-1 text-xl font-bold text-red-50">
                 Privacy Policy
             </h2>
-        </div>
+            <p className="text-lg"> RMN Media Hub GmbH, Hamburg, Germany (the "Company”), respects your privacy and is committed to protecting it through this privacy policy (the "policy”)</p>
+            <p className="text-lg">LAST UPDATED • JUNE 13, 2024</p>
+            <p className="text-lg">This policy describes the types of information the Company may collect from you or that you may provide when you visit the websites located at Rent Boys and rentboys.com (the "Websites”) and the Company's practices for collecting, using, keeping, protecting, and disclosing that informatio</p>
+            <p className="text-lg"></p>
+            <p className="text-lg"></p>
+            <p className="text-lg"></p>
+            <h3 className="text-lg font-semibold">— This policy applies to information the Company collects:</h3>
+            <ul className="list-disc [&>li]:ml-6 space-y-1">
+                <li>On the Website (including desktop and mobile versions).</li>
+                <li>In email, text, chat, and other electronic messages between you and the Company.</li>
+                <li> When you interact with the Company's advertising outlets and applications on third-party websites and services, if those applications or advertising include links to this policy.</li>
+                <li>When you interact with the Website through third-party social media platforms.</li>
+                <li>You give us license to use all media and information uploaded by you, see 8. MEMBER CONTENT for details.</li>
+            </ul>
+            <h3 className="text-lg font-semibold"> — It does not apply to information collected by:</h3>
+            <ul className="list-disc [&>li]:ml-6 space-y-1">
+                <li>The Company offline or through any other means, including on any other website operated by the Company or any third party (including the Company's affiliates and subsidiaries).</li>
+                <li> Or any third party (including the Company's affiliates and subsidiaries), including through any application or content (including advertising) that may link to or be accessible from (or on) the Website.</li>
+            </ul>
+            <p>Please read this policy carefully to understand the Company's policies and practices regarding your information and how the Company will treat it. If you do not agree with the Company's policies and practices, your choice is not to use the Website. By accessing or using the Website, you agree to this policy and consent to the Company's collection, use, disclosure, retention, and protection of your personal information as described in this policy. The Company may change this policy on one or more occasions. The Company will consider your continued use of the Website after the Company makes changes as your acceptance of the changes, so please check this policy frequently for updates.</p>
+            <div className="font-medium text-lg flex items-center gap-x-2">
+                <svg width="16" height="16" viewBox="0 0 8 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M0 4.75988V2.97988C0 0.77321 1.56667 -0.133456 3.48 0.973211L5.02 1.86654L6.56 2.75988C8.47333 3.86654 8.47333 5.67321 6.56 6.77988L5.02 7.67321L3.48 8.56654C1.56667 9.65321 0 8.75321 0 6.53988V4.75988Z" fill="#FF0000" />
+                </svg>
+                <h3 className="text-lg font-medium">Are minors welcome? No!</h3>
+            </div>
+            <p>
+                The Website is NOT intended for persons under 18-years old. You will only access the Website if (1) you are at least 18-years old and (2) have reached the age of majority where you live. The Company prohibits all persons who do not meet the age requirements from accessing the Website. Minors must not access the Website or use its services.
+            </p>
+            <p className="text-lg">
+                The Company does not knowingly collect any information about children, minors, or anyone under the age of majority. Nor does the Company knowingly market to children, minors, or anyone under 18-years old. If you are under 18-years old, you must not submit information to the Company and must leave the Website. If the Company becomes aware that a child, minor, or anyone under 18-years old has signed up with the Website and provided the Company with personal information, the Company will take steps to cancel that account. If the Company cancels an account because you are under 18-years old, the Company may keep your email and IP address to make sure that you do not try to get around the rules by creating a new account.
+            </p>
+            <p className="text-lg">
+                For US visitors in accordance with the Communications Decency Act 47 U.S.C. § 230(d), you are notified that parental control protections (including computer hardware, software, or filtering services) are commercially available that may assist in limiting access to material that is harmful to minors. You may find information about providers of these protections on the Internet by searching "parental control protection” or similar terms. If minors have access to your computer, please restrain their access to sexually explicit material by considering use of one or more of the following products, which the Company provides for informational purposes only and does not endorse:
+            </p>
+            <div className="flex flex-wrap gap-6 text-red-50 text-lg pt-6 pb-8 border-b border-gray-50">
+                <span className="underline">CYBERsitter™</span>
+                <span className="underline">Net Nanny®</span>
+                <span className="underline">CyberPatrol</span>
+                <span className="underline">ASACP</span>
+            </div>
+            <div className="font-medium text-lg flex items-center gap-x-2 pt-4">
+                <svg width="16" height="16" viewBox="0 0 8 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M0 4.75988V2.97988C0 0.77321 1.56667 -0.133456 3.48 0.973211L5.02 1.86654L6.56 2.75988C8.47333 3.86654 8.47333 5.67321 6.56 6.77988L5.02 7.67321L3.48 8.56654C1.56667 9.65321 0 8.75321 0 6.53988V4.75988Z" fill="#FF0000" />
+                </svg>
+                <h3 className="text-lg font-medium">What types of information does the Company collect about you and how is it collected?</h3>
+            </div>
+            <h3 className="text-lg font-semibold">— The Company may collect several types of information from and about users of the Website, including information:</h3>
+            <ul className="list-disc [&>li]:ml-6 space-y-1">
+                <li> By which you may be personally identified, including your name, postal address, email address, telephone number, credit card information,or any other information that the Company collects that is defined as personal or personally identifiable information under law ("personal information”).  </li>
+                <li>That is about you but individually does not identify you, including your age, gender, location, and any other optional information you provide about yourself.</li>
+                <li>Or about your Internet connection, the equipment you use to access the Website, and usage details.</li>
+            </ul>
+            <h3 className="text-lg font-semibold">— The Company collects this information:</h3>
+            <ul className="list-disc [&>li]:ml-6 space-y-1">
+                <li> Directly from you when you provide it to the Company.</li>
+                <li>Automatically as you navigate through the Website. Information collected automatically may include usage details, IP addresses, and information collected through cookies, web beacons, and other tracking technologies.</li>
+                <li>From third parties, for example, the Company's business partners.</li>
+            </ul>
+            <h3 className="text-lg font-semibold">— Information You Provide to the Company:</h3>
+            <p className="text-lg">The information the Company collects on or through the Website may include:</p>
+            <ul className="list-disc [&>li]:ml-6 space-y-1">
+                <li>Information that you provide by filing in forms on the Website. This includes information provided at the time of signing up to use the Website, purchasing services, requesting services, or engaging in advertising. The Company may also ask you for information when you report a problem with the Website or a service.</li>
+                <li>Records and copies of your correspondence (including email addresses), if you contact the Company.</li>
+                <li>Your responses to surveys that the Company might ask you to complete for research purposes.</li>
+                <li> Information you upload to the Website, for public viewing.</li>
+                <li>Details of transactions you carry out through the Website and of the fulfillment of your orders. You may be required to provide financial information before placing an order through the Website including for billing and collection, photo verification, ID verification and written authorizations.</li>
+                <li>   Your search queries on the Website.</li>
+            </ul>
+            <h3 className="text-lg font-semibold">— Information the Company Collects through Automatic Data Collection Technologies:</h3>
+            <p className="text-lg">As you navigate through and interact with the Website, the Company may use automatic data collection technologies to collect certain information about your equipment, browsing actions, and patterns, including:</p>
+            <ul className="list-disc [&>li]:ml-6 space-y-1">
+                <li>Details of your visits to the Website, including traffic data, location data, logs, and other communication data and the resources that you access and use on the Website.</li>
+                <li>Information about your computer and Internet connection, including your IP address, operating system, and browser type.
+                </li>
+                <li>Information about your mobile device and Internet connection, including the device's unique device identifier, IP address, operating system, browser type, mobile network information, and the device's telephone number. The Company also may use these technologies to collect information about your online activities over time and across third-party websites or other online services (behavioral tracking).</li>
+            </ul>
+            <h3 className="text-lg font-semibold">— The information the Company collects automatically is statistical data and does not include personal information, but the Company may keep it or associate it with personal information the Company collects in other ways or receive from third parties. It helps the Company to improve the Website and to deliver a better and more personalized service, including by allowing the Company to:</h3>
+            <ul className="list-disc [&>li]:ml-6 space-y-1">
+                <li>Estimate audience size and usage patterns.</li>
+                <li>Store information about your preferences, thus allowing the Company to customize the Website according to your individual interests.</li>
+                <li>Speed up your searches.</li>
+                <li>Recognize you when you return to the Website.</li>
+            </ul>
+            <h3 className="text-lg font-semibold">— The technologies the Company uses for this automatic data collection may include:</h3>
+
+            <ul className="list-disc [&>li]:ml-6 space-y-1">
+                <li>Cookies (or browser cookies): A cookie is a small file placed on the hard drive of your computer. Your browser stores cookies in a manner associated with each website you visit. The Company uses cookies to enable its servers to recognize your browser and tells the Company how and when you visit the Website and use the services. You may refuse to accept browser cookies by activating the appropriate setting on your browser. But if you select this setting you may be unable to access certain parts of the Website. Unless you have adjusted your browser setting so that it will refuse cookies, the Company's system will issue cookies when you direct your browser to the Website. If you want to learn more about cookies, please visit allaboutcookies.org  or www.youronlinechoices.eu</li>
+                <li>Flash Cookies: Certain features of the Website may use local stored objects (or Flash cookies) to collect and store information about your preferences and navigation to, from, and on the Website. Flash cookies are not managed by the same browser settings as are used for browser cookies. For information about managing your privacy and security settings for Flash cookies, you can access your Flash management tools from Adobe's website.</li>
+                <li>Speed up your searches.</li>
+                <li>Web Beacons: Pages of the Website (and the Company's emails) may contain small electronic files known as web beacons (also referred to as clear gifs, pixel tags, and single-pixel gifs) that permit the Company, for example, to count users who have visited those pages (or opened an email) and for other related website statistics (for example, recording the popularity of certain website content and verifying system and server integrity). If you want to learn more about web beacons, please visit allaboutcookies.org/web-beacons/.The Company does not collect personal information automatically, but it may tie this information to personal information about you that the Company collects from other sources or you provide to the Company.</li>
+
+            </ul>
+            <h3 className="text-lg font-semibold">— Third-party Use of Cookies and Other Tracking Technologies:</h3>
+
+            <ul className="list-disc [&>li]:ml-6 space-y-1">
+                <li>Some content or applications, including advertisements, on the Website are served by third parties, including advertisers, ad networks and servers, content providers, and application providers. These third parties may use cookies alone or in conjunction with web beacons or other tracking technologies to collect information about you when you use the Website. The information they collect may be associated with your personal information or they may collect information, including personal information, about your online activities over time and across different websites and other online services. They may use this information to provide you with interest-based (behavioral) advertising or other targeted content
+                </li>
+            </ul>
+            <p className="text-lg">
+                The Company does not control third-party tracking technologies or how third parties' use them. If you have any questions about an advertisement or other targeted content, you should contact the responsible provider directly.
+            </p>
+            {/* How does the Company use your information? */}
+        </div >
     );
 };
 
